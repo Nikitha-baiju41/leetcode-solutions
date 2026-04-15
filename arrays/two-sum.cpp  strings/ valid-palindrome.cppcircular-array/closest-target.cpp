9103343544374a438757e5890f1741ea41bd873f
NIKITHA BAIJU <nairnisha346@gmail.com>
2026-04-15 10:44:38 +0530

    Add closest target solution in circular array
    Implemented a solution to find the closest target in a circular array.

---
+++ arrays/two-sum.cpp  strings/ valid-palindrome.cppcircular-array/closest-target.cpp
@@ -0,0 +1,26 @@
+// Problem: Closest Target in Circular Array
+// Platform: LeetCode
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+        int n = words.size();
+        int ans = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            if (words[i] == target) {
+                int right = (i - startIndex + n) % n;
+                int left = (startIndex - i + n) % n;
+
+                int dist = min(right, left);
+                ans = min(ans, dist);
+            }
+        }
+
+        if (ans == INT_MAX) return -1;
+        return ans;
+    }
+};
